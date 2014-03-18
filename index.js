b2cc1649658f5f9b54d09405c88f4b3498ba24c6
Ben Drucker <bvdrucker@gmail.com>
2014-03-18 12:08:42 -0400

    Release

---
+++ index.js
@@ -0,0 +1,24 @@
+// Use chai-as-promised for promise assertions and attach the expect globally
+var chai = require('chai');
+chai.use(require('chai-as-promised'));
+global.expect = chai.expect;
+
+// Set up sinon
+var sinon = require('sinon');
+global.sinon = sinon;
+
+var Promise;
+
+// If bluebird is available, enable long stack traces and throw unhandled rejections
+// Also load sinon-as-promised to enable promise stubs
+try {
+  Promise = require('bluebird');
+} catch (e) {};
+
+if (Promise) {
+  Promise.longStackTraces();
+  Promise.onPossiblyUnhandledRejection(function (err) {
+    throw err;
+  });
+  require('sinon-as-promised')(Promise);
+}
